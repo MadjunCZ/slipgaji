66fd02ccd7cb8dd83af32480aad596660d805cb6
MadJunCZ <madjun120403@gmail.com>
2026-06-30 09:23:43 +0700

    Refactor application configuration and routing
    - Updated app.php to introduce maintenance mode configuration with support for "file" and "cache" drivers.
    - Modified cors.php to include 'sanctum/csrf-cookie' in the allowed paths.
    - Simplified index.php by removing maintenance mode checks and streamlining request handling.
    - Adjusted api.php to apply 'auth:sanctum' middleware to the user route.
    - Added comments in console.php for scheduling tasks.
    - Created new cache files for packages and services to optimize loading.
    - Introduced providers.php to manage application service providers.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,10 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -17,24 +16,19 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
-            Route::prefix('api')
-                ->middleware('api')
-                ->group(base_path('routes/api.php'));
+            //
         });
     }
 
@@ -43,10 +37,11 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function configureRateLimiting()
+    protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60);
+            $userId = $request->user()?->id;
+            return Limit::perMinute(60)->by($userId ?: $request->ip());
         });
     }
 }
